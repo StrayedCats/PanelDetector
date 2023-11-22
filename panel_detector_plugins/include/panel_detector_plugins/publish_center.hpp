27d3df60c082519618b171f6b9ce7557d8479ae8
Ar-Ray-code <ray255ar@gmail.com>
2023-11-23 01:08:36 +0900

    add publish_center.hpp

---
+++ panel_detector_plugins/include/panel_detector_plugins/publish_center.hpp
@@ -0,0 +1,14 @@
+#include <panel_detector_base/panel_detector_base.hpp>
+#include <panel_detector_param/panel_detector_param.hpp>
+#include <vision_msgs/msg/detection2_d_array.hpp>
+
+namespace panel_detector_plugins
+{
+typedef vision_msgs::msg::Detection2DArray Detection2DArray;
+class PublishCenter : public panel_detector_base::Detector
+{
+public:
+  void init(const panel_detector_parameters::ParamListener &) override;
+  Detection2DArray detect(const cv::Mat &) override;
+};
+}
--- panel_detector_plugins/src/publish_center.cpp
+++ panel_detector_plugins/src/publish_center.cpp
@@ -1,31 +1,24 @@
-#include <panel_detector_base/panel_detector_base.hpp>
-#include <panel_detector_param/panel_detector_param.hpp>
-#include <vision_msgs/msg/detection2_d_array.hpp>
+#include "panel_detector_plugins/publish_center.hpp"
 
 namespace panel_detector_plugins
 {
-class PublishCenter : public panel_detector_base::Detector
+
+void PublishCenter::init(const panel_detector_parameters::ParamListener & param_listener)
 {
-public:
-  void init(const panel_detector_parameters::ParamListener & param_listener) override
-  {
-    (void)param_listener;
-  }
-  vision_msgs::msg::Detection2DArray detect(const cv::Mat & image) override
-  {
-    int col = image.cols;
-    int row = image.rows;
-
-    vision_msgs::msg::Detection2DArray pose;
-    pose.detections.resize(1);
-    pose.detections[0].bbox.center.position.x = col / 2;
-    pose.detections[0].bbox.center.position.y = row / 2;
-    return pose;
-  }
-
-protected:
-  double side_length_;
-};
+  (void)param_listener;
 }
+Detection2DArray PublishCenter::detect(const cv::Mat & image)
+{
+  int col = image.cols;
+  int row = image.rows;
+
+  Detection2DArray pose;
+  pose.detections.resize(1);
+  pose.detections[0].bbox.center.position.x = col / 2;
+  pose.detections[0].bbox.center.position.y = row / 2;
+  return pose;
+}
+}
+
 #include <pluginlib/class_list_macros.hpp>
 PLUGINLIB_EXPORT_CLASS(panel_detector_plugins::PublishCenter, panel_detector_base::Detector)
