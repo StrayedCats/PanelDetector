f47f3536f194e9a4a45d4d70ecdb7ed5c7c8a80d
Ar-Ray-code <ray255ar@gmail.com>
2024-01-29 00:16:15 +0900

    Add detector2d_bringup package with launch files

---
+++ detector2d_bringup/launch/detector_yolox.launch.py
@@ -0,0 +1,62 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+def generate_launch_description():
+    container = ComposableNodeContainer(
+        name='detector_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='realsense2_camera',
+                plugin='realsense2_camera::RealSenseNodeFactory',
+                name='realsense2_camera',
+                namespace='',
+                parameters=[{
+                    'align_depth.enable': True,
+                    'enable_color': True,
+                    'enable_depth': True
+                }]
+            ),
+            ComposableNode(
+                package='detector2d_node',
+                plugin='detector2d_node::Detector2dNode',
+                name='detector2d_node',
+                namespace='',
+                parameters=[{
+                    'yolox_trt_plugin.model_path': os.path.expanduser('~') + '/yolox_tiny.trt',
+                    'load_target_plugin': 'detector2d_plugins::YoloxTrt'
+                }],
+                remappings=[
+                    ('image_raw', 'color/image_raw')
+                ]
+            ),
+            ComposableNode(
+                package='bbox2d_to_3d_node',
+                plugin='bbox2d_to_3d_node::BBox2DTo3DNode',
+                name='bbox2d_to_3d_node',
+                namespace='',
+                parameters=[{
+                    'min_depth': 0.05,
+                    'max_depth': 3.0,
+                    'imshow_isshow': True
+                }],
+                remappings=[
+                    ('bbox2d', 'positions'),
+                    ('camera_info', 'aligned_depth_to_color/camera_info'),
+                    ('depth', 'aligned_depth_to_color/image_raw'),
+                    ('color', 'color/image_raw')
+                ]
+            ),
+        ]
+    )
+
+    return LaunchDescription([
+        container
+    ])
